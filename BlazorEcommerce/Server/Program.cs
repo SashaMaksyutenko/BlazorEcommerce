f41863f7a928177c4994360bdac708b0ad2e5816
Максютенко Саша <Sasha.Maksyutenko@gmail.com>
2022-09-18 17:47:16 +0300

    product details, categories and seeding more products

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,7 +1,10 @@
 global using BlazorEcommerce.Shared;
 global using Microsoft.EntityFrameworkCore;
 global using BlazorEcommerce.Server.Data;
+global using BlazorEcommerce.Server.Services.ProductService;
+
 using Microsoft.AspNetCore.ResponseCompression;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +16,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IProductService, ProductService>();
 var app = builder.Build();
 app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
