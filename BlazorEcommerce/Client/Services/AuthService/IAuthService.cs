f24d9c142cbdc21feb7792d5fc6175874c71b941
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2022-10-26 00:35:52 +0300

    commit14
    login implementations

--- BlazorEcommerce/Client/Services/AuthService/IAuthService.cs
+++ BlazorEcommerce/Client/Services/AuthService/IAuthService.cs
@@ -2,6 +2,7 @@
 {
     public interface IAuthService
     {
-        Task<ServiceResponse<int>> Register(UserRegister request);     
+        Task<ServiceResponse<int>> Register(UserRegister request);
+        Task<ServiceResponse<string>> Login(UserLogin request);
     }
 }
