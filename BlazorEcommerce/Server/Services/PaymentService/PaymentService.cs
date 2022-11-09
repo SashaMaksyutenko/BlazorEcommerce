7e8185e747eb8a6cf032923ac83d0edd99a48b76
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2022-11-09 18:04:13 +0200

    commit21
    for stripe business account required

--- BlazorEcommerce/Server/Services/PaymentService/PaymentService.cs
+++ BlazorEcommerce/Server/Services/PaymentService/PaymentService.cs
@@ -3,26 +3,29 @@ using Stripe.Checkout;
 
 namespace BlazorEcommerce.Server.Services.PaymentService
 {
-    public class PaymentService:IPaymentService
+    public class PaymentService : IPaymentService
     {
         private readonly ICartService _cartService;
         private readonly IAuthService _authService;
         private readonly IOrderService _orderService;
+
         const string secret = "sk_test_51LzQzCDhXjHNMx1O5bqxSlcQ9Gt6LvFS2oOlwD9x1OFc9c3C3kN03N0js73JF9gf9MU7lGRHiFMQXQtsUU6DioEe00i5OHpuO3";
+
         public PaymentService(ICartService cartService,
             IAuthService authService,
-           IOrderService orderService)
+            IOrderService orderService)
         {
-            StripeConfiguration.ApiKey = "pk_test_51LzQzCDhXjHNMx1O5sZNpzS56Q1DXyxeX5NEI5OZAuMsPyZWE4oJaqjweJHHFFwyQU73dy6xBOoh3mljPQXX3ggW00MJCQ5sL1";
-            _cartService =cartService;
-            _authService=authService;
-            _orderService=orderService;
+            StripeConfiguration.ApiKey = "sk_test_51LzQzCDhXjHNMx1O5bqxSlcQ9Gt6LvFS2oOlwD9x1OFc9c3C3kN03N0js73JF9gf9MU7lGRHiFMQXQtsUU6DioEe00i5OHpuO3";
+
+            _cartService = cartService;
+            _authService = authService;
+            _orderService = orderService;
         }
 
         public async Task<Session> CreateCheckoutSession()
         {
             var products = (await _cartService.GetDbCartProducts()).Data;
-            var lineItems=new List<SessionLineItemOptions>();
+            var lineItems = new List<SessionLineItemOptions>();
             products.ForEach(product => lineItems.Add(new SessionLineItemOptions
             {
                 PriceData = new SessionLineItemPriceDataOptions
@@ -37,14 +40,15 @@ namespace BlazorEcommerce.Server.Services.PaymentService
                 },
                 Quantity = product.Quantity
             }));
+
             var options = new SessionCreateOptions
             {
                 CustomerEmail = _authService.GetUserEmail(),
                 ShippingAddressCollection =
-                new SessionShippingAddressCollectionOptions
-                {
-                    AllowedCountries = new List<string> { "US" }
-                },
+                    new SessionShippingAddressCollectionOptions
+                    {
+                        AllowedCountries = new List<string> { "US" }
+                    },
                 PaymentMethodTypes = new List<string>
                 {
                     "card"
@@ -54,6 +58,7 @@ namespace BlazorEcommerce.Server.Services.PaymentService
                 SuccessUrl = "https://localhost:7184/order-success",
                 CancelUrl = "https://localhost:7184/cart"
             };
+
             var service = new SessionService();
             Session session = service.Create(options);
             return session;
@@ -61,25 +66,27 @@ namespace BlazorEcommerce.Server.Services.PaymentService
 
         public async Task<ServiceResponse<bool>> FulfillOrder(HttpRequest request)
         {
-            var json=await new StreamReader(request.Body).ReadToEndAsync();
+            var json = await new StreamReader(request.Body).ReadToEndAsync();
             try
             {
                 var stripeEvent = EventUtility.ConstructEvent(
-                    json,
-                    request.Headers["Stripe-Signature"],
-                    secret
+                        json,
+                        request.Headers["Stripe-Signature"],
+                        secret
                     );
-                if(stripeEvent.Type==Events.CheckoutSessionCompleted)
+
+                if (stripeEvent.Type == Events.CheckoutSessionCompleted)
                 {
                     var session = stripeEvent.Data.Object as Session;
                     var user = await _authService.GetUserByEmail(session.CustomerEmail);
                     await _orderService.PlaceOrder(user.Id);
                 }
+
                 return new ServiceResponse<bool> { Data = true };
             }
-            catch(StripeException e)
+            catch (StripeException e)
             {
-                return new ServiceResponse<bool> { Data = false,Success=false,Message=e.Message };
+                return new ServiceResponse<bool> { Data = false, Success = false, Message = e.Message };
             }
         }
     }
